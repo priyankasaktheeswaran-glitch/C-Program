91a2b6bcf050d3b32daa93fb7d81dcbfc01de0e3
Priyanka Saktheeswaran <priyankasaktheeswaran@gmail.com>
2026-02-06 06:51:40 +0530

    Refactor array counting program with dynamic size

--- array 2.c
+++ array 2.c
@@ -1,14 +1,18 @@
-#include <stdio.h>
-int main(){
-    int n,a[100],x,count=0;
-    scanf("%d",&n);
-    for(int i=0;i<n;i++)
-        scanf("%d",&a[i]);
-    scanf("%d",&x);
-    for(int i=0;i<n;i++)
-        if(a[i]==x)
+#include<stdio.h>
+int main() {
+    int size;
+    scanf("%d",&size);
+    int arr[size];
+    for (int i=0;i<size;i++) {
+        scanf("%d",&arr[i]);
+    }
+    int X,count=0;
+    scanf("%d",&X);
+    for (int i=0;i<size;i++) {
+        if (arr[i]==X) {
             count++;
+        }
+    }
     printf("%d",count);
     return 0;
 }
-
