657bc866a7a02dd1ae2a75a18afb3ba6be25d7b4
Priyanka Saktheeswaran <priyankasaktheeswaran@gmail.com>
2026-02-11 23:14:09 +0530

    Add files via upload

---
+++ string6.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+int main() {
+    char str[100];
+    fgets(str,sizeof(str),stdin);
+    int count=0;
+    for(int i=0;str[i]!='\0';i++) {
+        if (str[i]=='a' || str[i]=='e' || str[i]=='i' ||
+            str[i]=='o' || str[i]=='u') {
+            count++;
+            }
+    }
+    printf("%d",count);
+    return 0;
+}
