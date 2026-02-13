f83c811a4b310b474c786c5eb0647fc11a989d65
Priyanka Saktheeswaran <priyankasaktheeswaran@gmail.com>
2026-02-13 22:32:29 +0530

    Add files via upload

---
+++ string15.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+int main() {
+    char str[100];
+    int count=0,words=0;
+    fgets(str,sizeof(str),stdin);
+    for (int i=0;str[i]!='\0';i++) {
+        if (str[i]==' ') {
+            count++;
+            words=count+1;
+        }
+    }
+    printf("%d",words);
+    return 0;
+}
