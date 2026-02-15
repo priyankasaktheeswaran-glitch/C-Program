28722ccca935e2e1fe5523c9059f4772ca4acd1e
Priyanka Saktheeswaran <priyankasaktheeswaran@gmail.com>
2026-02-15 21:37:19 +0530

    Add files via upload

---
+++ fun13.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int power(int a,int b) {
+    int result=1;
+    for (int i=1;i<=b;i++) {
+        result=result*a;
+    }
+    return result;
+}
+
+int main() {
+    int a,b;
+    printf("enter two integers:");
+    scanf("%d%d",&a,&b);
+    printf("%d",power(a,b));
+    return 0;
+}
