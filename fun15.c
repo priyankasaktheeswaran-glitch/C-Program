9f2443060f0756d6d41ef939c7e8f7d6029e5724
Priyanka Saktheeswaran <priyankasaktheeswaran@gmail.com>
2026-02-15 21:37:50 +0530

    Add files via upload

---
+++ fun15.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int table(int n) {
+    for (int i = 1; i <= 10; i++) {
+        printf("%d*%d=%d\n",n,i,n*i);
+    }
+}
+int main() {
+    int n;
+    printf("enter the number:");
+    scanf("%d",&n);
+    table(n);
+    return 0;
+
+}
