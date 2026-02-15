8f0afd70baa1a5f4e6ea80b74b1dc1a1fdf392ce
Priyanka Saktheeswaran <priyankasaktheeswaran@gmail.com>
2026-02-15 22:48:44 +0530

    Add files via upload

---
+++ pointers05.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int sum(int *arr,int size) {
+    int sum=0;
+    for(int i=0;i<size;i++) {
+        sum=sum+arr[i];
+    }
+    return sum;
+}
+int main() {
+    int size;
+    scanf("%d",&size);
+    int arr[size];
+    for(int i=0;i<size;i++) {
+        scanf("%d",&arr[i]);
+    }
+    int total=sum(arr,size);
+    printf("%d",total);
+    return 0;
+}
